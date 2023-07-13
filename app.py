82c7382c6c01259c2b80bfb3d60e7325e59e53ef
zulmaryleon <139059574+zulmaryleon@users.noreply.github.com>
2023-07-13 19:30:10 -0400

    actualizacion2
    inicio de sesion, botones, creacion de 3 ventanas

--- app.py
+++ app.py
@@ -1,31 +1,112 @@
 import tkinter as tk
 from funciones.funcionesdebotones import redirigir_inicio, redirigir_inventario, redirigir_productos, redirigir_usuario
+from tkinter import messagebox
 
+#metodo del boton1
+def boton1(): 
+    ventana_boton1=tk.Toplevel()
+    ventana_boton1.title ('boton1')
+    ventana_boton1.geometry("800x600")
+    ventana_boton1.configure(bg="purple")
+    ventana_boton1.mainloop()
+    
+
+# Metodo de inicio de session
+def iniciar_sesion():
+    usuario = usuario_entry.get()
+    contrasena = password_entry.get()
+
+    # Verificar las credenciales de inicio de sesión
+    if usuario == "admin" and contrasena == "123456":
+        messagebox.showinfo("Inicio de sesión exitoso", f"Bienvenido, {usuario}!")
+        ventana.withdraw()  # Ocultar la ventana de inicio de sesión
+
+        # Obtener el ancho y alto de la pantalla
+        ancho_pantalla = ventana.winfo_screenwidth()
+        alto_pantalla = ventana.winfo_screenheight()
+
+        # Abrir la ventana del panel
+        ventana_dashboard = tk.Toplevel()
+        ventana_dashboard.title("Panel")
+        ventana_dashboard.configure(bg="white")
+
+        # Establecer el tamaño y posición de la ventana
+        ventana_dashboard.geometry(f"{ancho_pantalla}x{alto_pantalla}+0+0")
+
+        # Panel vertical izquierdo (Menú)
+        panel_izquierdo = tk.Frame(ventana_dashboard, bg="light gray")
+        panel_izquierdo.pack(side="left", fill="y")
+
+        # Elementos del menú
+        etiqueta_menu = tk.Label(panel_izquierdo, text="Menú", bg="light gray", font=("Arial", 16, "bold"))
+        etiqueta_menu.pack(pady=10)
+
+        # Opción 1
+        opcion1 = tk.Button(panel_izquierdo, text="Opción 1", bg="white", padx=10, command=boton1, pady=5)
+        opcion1.pack(pady=5)
+        
+
+        # Opción 2
+        opcion2 = tk.Button(panel_izquierdo, text="Opción 2", bg="white", padx=10, pady=5)
+        opcion2.pack(pady=5)
+
+        # Opción 3
+        opcion3 = tk.Button(panel_izquierdo, text="Opción 3", bg="white", padx=10, pady=5)
+        opcion3.pack(pady=5)
+
+        # Contenedor a la derecha
+        contenedor_derecho = tk.Frame(ventana_dashboard, bg="white")
+        contenedor_derecho.pack(side="right", fill="both", expand=True)
+
+        # Agregar elementos al contenedor derecho
+        etiqueta_titulo = tk.Label(contenedor_derecho, text="Contenido", bg="white", font=("Arial", 16, "bold"))
+        etiqueta_titulo.pack(pady=10)
+
+        contenido = tk.Label(contenedor_derecho, text="Aquí va el contenido del menú seleccionado", bg="white")
+        contenido.pack(pady=5)
+
+
+
+        ventana_dashboard.mainloop()
+    else:
+        messagebox.showerror("Inicio de sesión fallido", "Credenciales incorrectas")
+
+# Ventana principal
 ventana=tk.Tk()
 ventana.title("sistema de inventario")
-ventana.geometry("1200x800")
+ventana.geometry("800x400")
 
-navbar=tk.Frame(ventana,bg="pink")
-navbar.pack(side="top", fill="x")
+# Agregar un marco que ocupe todo el espacio
+marco = tk.Frame(ventana, bg="white")
+marco.place(relwidth=1, relheight=1)
 
-fondo=tk.Label(navbar, bg="pink")
-fondo.pack(side="left")
+# Crear un marco para el formulario
+formulario_frame = tk.Frame(marco, bg="pink", bd=5)
+formulario_frame.place(relx=0.5, rely=0.5, relwidth=0.3, relheight=0.3, anchor="center")
 
-#botonesizquierdos
-boton1=tk.Button(navbar,text="inicio", command=redirigir_inicio)
-boton1.pack(side="left")
-boton2=tk.Button(navbar,text="productos", command=redirigir_productos)
-boton2.pack(side="left")
+# Etiqueta de usuario
+usuario_label = tk.Label(formulario_frame, text="Usuario:", bg="black", fg="white")
+usuario_label.place(relx=0.1, rely=0.2, relwidth=0.3, relheight=0.2)
 
-#botonesderechos
-boton3=tk.Button(navbar,text="usuario", command=redirigir_usuario)
-boton3.pack(side="right")
-boton4=tk.Button(navbar,text="inventario", command=redirigir_inventario)
-boton4.pack(side="right")
+# Cuadro de entrada de usuario
+usuario_entry = tk.Entry(formulario_frame, bg="white")
+usuario_entry.place(relx=0.4, rely=0.2, relwidth=0.5, relheight=0.2)
+
+# Etiqueta de contraseña
+password_label = tk.Label(formulario_frame, text="Contraseña:", bg="black", fg="white")
+password_label.place(relx=0.1, rely=0.5, relwidth=0.3, relheight=0.2)
+
+# Cuadro de entrada de contraseña
+password_entry = tk.Entry(formulario_frame, show="*", bg="white")
+password_entry.place(relx=0.4, rely=0.5, relwidth=0.5, relheight=0.2)
+
+# Botón de inicio de sesión
+boton_iniciar_sesion = tk.Button(formulario_frame, text="Iniciar sesión", command=iniciar_sesion, activebackground="#F50743")
+boton_iniciar_sesion.place(relx=0.4, rely=0.8, relwidth=0.5, relheight=0.2)
 
 
 
-
+# Iniciar el bucle principal de la ventana
 ventana.mainloop()
 
 ventana.quit()
